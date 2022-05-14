09a9a1920ffeca595242521cd6ef1fa1a5a5b968
AlnisS <alnis.smidchens@gmail.com>
2022-05-14 16:49:43 -0700

    Improved visuals (night lighting etc.)

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+
+var last_mouse_position = Vector2.ZERO
+
+
+func _physics_process(delta):
+	$Axis.rotate_y(delta * 0.02)
+	
+	var mouse_delta = get_viewport().get_mouse_position() - last_mouse_position
+	
+	if Input.is_action_pressed("rotate"):
+		$CameraBase.rotation_degrees.x = clamp(
+			$CameraBase.rotation_degrees.x - mouse_delta.y * 0.4,
+			-90.0, 90.0
+		)
+		$CameraBase.rotation_degrees.y -= mouse_delta.x * 0.4
+	
+	last_mouse_position = get_viewport().get_mouse_position()
