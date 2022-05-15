b3301ff2a26fe17b6956e71f0f025f4aa1c8685f
AlnisS <alnis.smidchens@gmail.com>
2022-05-14 19:22:38 -0700

    Adding UI and tweaking some stuff

--- main.gd
+++ main.gd
@@ -1,11 +1,21 @@
 extends Spatial
 
 
+var default_env = preload("res://default_env.tres")
+var camera_space_env = preload("res://camera_space_env.tres")
+
+
 var last_mouse_position = Vector2.ZERO
 
 
 func _physics_process(delta):
-	$Earth.rotate_y(delta * 0.02)
+	if $VBoxContainer/SpinningGlobe/CheckButton.pressed:
+		$EarthBase/Earth.rotation.y += delta * 0.1
+	
+	if $VBoxContainer/FullVisibility/CheckButton.pressed:
+		$CameraBase/Camera.environment = default_env
+	else:
+		$CameraBase/Camera.environment = camera_space_env
 	
 	var mouse_delta = get_viewport().get_mouse_position() - last_mouse_position
 	
